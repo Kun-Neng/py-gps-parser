52b351184e418e2c926530a023df3b272c3b9af2
Kun-Neng <kunneng.hung@gmail.com>
2021-12-23 15:21:18 +0800

    add datetime parser method to construct correct format of datetime

--- main/parser.py
+++ main/parser.py
@@ -18,14 +18,54 @@ class Parser:
             if one_line[0] != '$':
                 continue
 
-            if (one_line.find("*") == -1):
+            if (one_line.find('*') == -1):
                 continue
 
             if (one_line.find('RMC') != -1):
-                print("%v", one_line)
+                # print(one_line)
+                self.parse_datetime(one_line)
                 flag_latlon_ready = True
 
         if flag_latlon_ready is True:
             self.is_GPS_normal = True
         
         return self.is_GPS_normal
+    
+    def parse_date(self, date_token) -> str:
+        # UTC Date format ddmmyy
+        year = int('20' + date_token[4:6])
+        month = int(date_token[2:4])
+        day = int(date_token[0:2])
+
+        month_str = f'0{month}' if month < 10 else f'{month}'
+        day_str = f'0{day}' if day < 10 else f'{day}'
+        # print(f'{year}-{month_str}-{day_str}')
+
+        return f'{year}-{month_str}-{day_str}'
+    
+    def parse_time(self, time_token) -> str:
+        # UTC time format hhmmss.sss (000000.000 ~ 235959.999)
+        hour = int(time_token[:2])
+        minute = int(time_token[2:4])
+        second = int(time_token[4:6])
+        # msec = int(time_token[7:9])
+        # nsec = 1000 * msec
+
+        hour_str = f'0{hour}' if hour < 10 else f'{hour}'
+        minute_str = f'0{minute}' if minute < 10 else f'{minute}'
+        second_str = f'0{second}' if second < 10 else f'{second}'
+        # print(f'{hour_str}:{minute_str}:{second_str}')
+
+        return f'{hour_str}:{minute_str}:{second_str}'
+    
+    def parse_datetime(self, one_line):
+        str_slice = one_line.split(',')
+
+        time_token = str_slice[1]
+        date_token = str_slice[9]
+
+        if len(date_token) < 6:
+            return None
+
+        time_string = self.parse_date(date_token) + ' ' + self.parse_time(time_token)
+        print(time_string)
--- tests/test_parser/test_parser.py
+++ tests/test_parser/test_parser.py
@@ -1,14 +1,42 @@
 from main.parser import Parser
 
 
-invalid_RMC_message_1 = f'GNRMC,060123.00,A,2451.75159,N,12115.52450,E,0.387,,271021,,,A*6E'
-invalid_RMC_message_2 = f'$GNRMC,060123.00,A,2451.75159,N,12115.52450,E,0.387,,271021,,,A'
-normal_RMC_message = f'$GNRMC,060123.00,A,2451.75159,N,12115.52450,E,0.387,,271021,,,A*6E'
+invalid_RMC_1 = 'GNRMC,062357.00,A,2451.76062,N,12115.52127,E,0.132,,231221,,,A*68'
+invalid_RMC_2 = '$GNRMC,062357.00,A,2451.76062,N,12115.52127,E,0.132,,231221,,,A'
+normal_RMC = '$GNRMC,062357.00,A,2451.76062,N,12115.52127,E,0.132,,231221,,,A*68'
+
+initial_normal_message = \
+'$GNRMC,055020.00,V,,,,,,,231221,,,N*60' \
+'$GNVTG,,,,,,,,,N*2E' \
+'$GNGGA,055020.00,,,,,0,00,99.99,,,,,,*7A' \
+'$GNGSA,A,1,,,,,,,,,,,,,99.99,99.99,99.99*2E' \
+'$GNGSA,A,1,,,,,,,,,,,,,99.99,99.99,99.99*2E' \
+'$GPGSV,3,1,09,03,11,316,,10,19,180,,16,22,220,17,22,23,296,21*7B' \
+'$GPGSV,3,2,09,25,23,039,26,26,61,239,34,29,28,081,,31,51,345,*79' \
+'$GPGSV,3,3,09,32,70,096,*49' \
+'$GLGSV,3,1,09,65,63,320,,66,05,333,,71,13,156,,72,62,164,*6B' \
+'$GLGSV,3,2,09,74,20,031,,75,52,332,,76,29,260,,84,03,070,*6F' \
+'$GLGSV,3,3,09,85,03,123,*52' \
+'$GNGL'
+
 
 
 def test_parse_NMEA():
     parser = Parser()
     assert parser.parse_NMEA('') == False
-    assert parser.parse_NMEA(invalid_RMC_message_1) == False
-    assert parser.parse_NMEA(invalid_RMC_message_2) == False
-    assert parser.parse_NMEA(normal_RMC_message) == True
+    assert parser.parse_NMEA(invalid_RMC_1) == False
+    assert parser.parse_NMEA(invalid_RMC_2) == False
+    # assert parser.parse_NMEA(normal_RMC) == True
+    # assert parser.parse_NMEA(initial_normal_message) == True
+
+def test_parse_date():
+    parser = Parser()
+    assert parser.parse_date('231221') == '2021-12-23'
+
+def test_parse_time():
+    parser = Parser()
+    assert parser.parse_time('062357.00') == '06:23:57'
+
+def test_parse_datetime():
+    parser = Parser()
+    parser.parse_datetime(normal_RMC)
