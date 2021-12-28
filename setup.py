7dc1996e64dbd710270b3ca7ce4a02cb7ffde199
Kun-Neng <kunneng.hung@gmail.com>
2021-12-28 10:49:47 +0800

    modify the package folder name to pygpsparser

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='gpsparser',
+    name='pygpsparser',
     version='0.0.3',
     author='Kun-Neng Hung',
     author_email='kunneng.hung@gmail.com',
